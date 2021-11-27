dc543324f7c3bda3eba25f27c02da127730fa3bb
coolkim99 <coolkim99@sookmyung.ac.kr>
2021-11-28 03:19:47 +0900

    [FEAT] 작성 페이지 퍼스널 인포 완료

--- src/components/Write/PersonInfo.js
+++ src/components/Write/PersonInfo.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import Button from '../common/Button';
 
 const Wrapper = styled.div`
     width : 1100px;
@@ -8,22 +9,70 @@ const Wrapper = styled.div`
     border-radius : 5px;
     margin-bottom : 30px;
     padding : 24px;
+    display : flex;
+`
+
+const Info = styled.div`
+    display: flex;
+    width : 1000px;
+`
+
+const ProgramWrapper = styled.div`
+    height : 20px;
+    width : 100px;
+    border-right : 0.1px gray solid;
+    margin-right : 30px;
 `
 
 const Program = styled.div`
-    
 `
 
 const Name = styled.div`
-    
+    margin : 0 30px;
+    display : inline-block;
+`
+const Birthday = styled.div`
+    margin : 0 30px;
+`
+const Parent = styled.div`
+    margin : 0 30px;
+`
+
+const Phone = styled.div`
+    margin : 0 30px;
+`
+const Relationship = styled.div`
+    margin : 0 30px;
+`
+
+const Recode = styled(Button)`
+    background : white;
+    color : black;
+    border : 0.1px gray solid;
+    padding : 0.5px;
+    font-size : 0.9em;
+    font-weight: normal;
+    border-radius: 2px;
+    margin-right : 4px;
+    display : inline-block;
+
 `
 
 const PersonInfo = () => {
     return(
     <>
         <Wrapper>
-            <Program>프로그램 명</Program>
-            <Name> 환자 이름</Name>
+            <Info>
+                <ProgramWrapper>
+                    <Program>프로그램 명</Program>
+                </ProgramWrapper>
+                <Name>환자 이름</Name>
+                <Birthday>생년월일</Birthday>
+                <Parent>보호자 성함</Parent>
+                <Relationship>보호자 관계</Relationship>
+                <Phone>000-0000-0000</Phone>
+            </Info>
+            <Recode>가정일지 조회</Recode>
         </Wrapper>
     </>
     );
